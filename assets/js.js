ffb2203af650099f15d874a22882806dc89e8a9d
coolJenny <31550194+coolJenny@users.noreply.github.com>
2017-10-12 10:24:22 -0400

    Auto-generated commit

---
+++ assets/js.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 300) {
+            $('.bfooter').fadeIn();
+        } else {
+            $('.bfooter').fadeOut();
+        }
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 400) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},"1000");
+        return false;
+    });
+});
